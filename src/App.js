56b2df1f7ece460b989fa771a006368cbec3038f
Brendan McCaughey <brendan.mccaughey@workgrid.com>
2021-09-22 11:50:14 -0400

    Editor is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { EditorState } from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-function App() {
+
+const App = () => {
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Rich Text Editor
       </header>
+        <Editor editorState={editorState}
+                onEditorStateChange={setEditorState}
+                wrapperClassName="wrapper-class"
+                editorClassName="editor-class"
+                toolbarClassName="toolbar-class"/>
     </div>
   );
 }
