7d9b6ffb540bb47d15a33f21f163fba5f409fb9f
Brendan McCaughey <brendan.mccaughey@workgrid.com>
2021-09-22 14:14:15 -0400

    Added AST output

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import './App.css';
 import { useState } from 'react'
-import { EditorState } from 'draft-js'
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { EditorState} from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import exporter from 'draft-js-ast-exporter'
 
 
 const App = () => {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
+
+  const ast = exporter(editorState)
+  
   return (
     <div className="App">
+      <div className="column">
       <header className="App-header">
-        Rich Text Editor
+        Rich Text
       </header>
         <Editor editorState={editorState}
                 onEditorStateChange={setEditorState}
-                wrapperClassName="wrapper-class"
                 editorClassName="editor-class"
                 toolbarClassName="toolbar-class"/>
+      </div>
+      <div className="column">
+      <header className="App-header">
+        AST Output
+      </header>
+      <pre className="code-block">
+        {JSON.stringify(
+          ast,
+          null,
+          2
+        )}
+      </pre>
+      </div>
     </div>
   );
 }
